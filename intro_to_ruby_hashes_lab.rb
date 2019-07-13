fd7ffe668f579dd9ab111324703c105753787c93
Aicha D <aicha1diallo@gmail.com>
2019-07-13 17:12:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,11 +20,11 @@ end
 
 def read_from_hash(hash, key)
   # {:hash => 'key'}
-hash[key]
+  hash[key]
 end 
 
-# def update_counting_hash(hash, key)
-#   {:hash => key
-#     hash +1
-#   }
-# end
+def update_counting_hash(hash, key)
+  {:hash => key
+    hash +1
+  }
+end
